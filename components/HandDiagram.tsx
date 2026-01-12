32cdd3adb0462e89416913bc30f5c0a9a7d7d7ca
Veronica Ray <veronica.l.ray@gmail.com>
2026-01-11 18:14:14 -0800

    Fix hand diagram: correct finger positions (pinky left on left hand, thumb left on right hand)

--- components/HandDiagram.tsx
+++ components/HandDiagram.tsx
@@ -5,39 +5,40 @@ interface HandDiagramProps {
   activeFinger: number | null // 0=thumb, 1=index, 2=middle, 3=ring, 4=pinky
 }
 
-// SVG paths for each finger of the left hand
+// SVG paths for each finger of the LEFT hand (viewed from above while typing)
+// Pinky is on the LEFT, thumb is on the RIGHT
 // Fingers indexed: 0=thumb, 1=index, 2=middle, 3=ring, 4=pinky
 const LEFT_HAND_FINGERS = [
-  // Thumb
-  "M 45 85 Q 30 75 25 60 Q 22 50 28 42 Q 35 35 45 40 Q 52 45 52 55 L 50 75 Z",
-  // Index
-  "M 55 55 L 55 20 Q 55 12 62 12 Q 69 12 69 20 L 69 55 Z",
-  // Middle
-  "M 72 55 L 72 15 Q 72 7 79 7 Q 86 7 86 15 L 86 55 Z",
-  // Ring
-  "M 89 55 L 89 20 Q 89 12 96 12 Q 103 12 103 20 L 103 55 Z",
-  // Pinky
-  "M 106 55 L 106 30 Q 106 22 113 22 Q 120 22 120 30 L 120 55 Z",
+  // Thumb (rightmost, angled outward)
+  "M 115 80 Q 125 70 130 55 Q 132 45 125 40 Q 118 35 110 42 Q 105 50 107 60 L 110 75 Z",
+  // Index (second from right)
+  "M 95 55 L 95 18 Q 95 10 88 10 Q 81 10 81 18 L 81 55 Z",
+  // Middle (center, tallest)
+  "M 78 55 L 78 12 Q 78 4 71 4 Q 64 4 64 12 L 64 55 Z",
+  // Ring (second from left)
+  "M 61 55 L 61 18 Q 61 10 54 10 Q 47 10 47 18 L 47 55 Z",
+  // Pinky (leftmost, shortest)
+  "M 44 55 L 44 28 Q 44 20 37 20 Q 30 20 30 28 L 30 55 Z",
 ]
 
-// Palm base
-const LEFT_PALM = "M 50 55 L 50 95 Q 50 110 65 115 Q 85 120 105 115 Q 120 110 120 95 L 120 55 Q 110 50 85 50 Q 60 50 50 55 Z"
+const LEFT_PALM = "M 30 55 L 30 95 Q 30 110 50 115 Q 75 120 100 115 Q 115 108 115 90 L 112 70 Q 100 50 70 50 Q 40 50 30 55 Z"
 
-// Mirror for right hand (flip horizontally around center)
+// SVG paths for each finger of the RIGHT hand (viewed from above while typing)
+// Thumb is on the LEFT, pinky is on the RIGHT
 const RIGHT_HAND_FINGERS = [
-  // Thumb (mirrored)
-  "M 105 85 Q 120 75 125 60 Q 128 50 122 42 Q 115 35 105 40 Q 98 45 98 55 L 100 75 Z",
-  // Index
-  "M 95 55 L 95 20 Q 95 12 88 12 Q 81 12 81 20 L 81 55 Z",
-  // Middle
-  "M 78 55 L 78 15 Q 78 7 71 7 Q 64 7 64 15 L 64 55 Z",
-  // Ring
-  "M 61 55 L 61 20 Q 61 12 54 12 Q 47 12 47 20 L 47 55 Z",
-  // Pinky
-  "M 44 55 L 44 30 Q 44 22 37 22 Q 30 22 30 30 L 30 55 Z",
+  // Thumb (leftmost, angled outward)
+  "M 35 80 Q 25 70 20 55 Q 18 45 25 40 Q 32 35 40 42 Q 45 50 43 60 L 40 75 Z",
+  // Index (second from left)
+  "M 55 55 L 55 18 Q 55 10 62 10 Q 69 10 69 18 L 69 55 Z",
+  // Middle (center, tallest)
+  "M 72 55 L 72 12 Q 72 4 79 4 Q 86 4 86 12 L 86 55 Z",
+  // Ring (second from right)
+  "M 89 55 L 89 18 Q 89 10 96 10 Q 103 10 103 18 L 103 55 Z",
+  // Pinky (rightmost, shortest)
+  "M 106 55 L 106 28 Q 106 20 113 20 Q 120 20 120 28 L 120 55 Z",
 ]
 
-const RIGHT_PALM = "M 100 55 L 100 95 Q 100 110 85 115 Q 65 120 45 115 Q 30 110 30 95 L 30 55 Q 40 50 65 50 Q 90 50 100 55 Z"
+const RIGHT_PALM = "M 120 55 L 120 95 Q 120 110 100 115 Q 75 120 50 115 Q 35 108 35 90 L 38 70 Q 50 50 80 50 Q 110 50 120 55 Z"
 
 const FINGER_NAMES = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
 
